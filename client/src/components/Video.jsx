1296d5f71294a38586b4a9a18494307950e9a150
Deepanshu Chauhan <deepanshuchauhan483@gmail.com>
2024-09-27 19:23:39 +0530

    complete functionality

---
+++ client/src/components/Video.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Video() {
+  return (
+    <div><video autoplay muted loop id ="myVideo" src="rain.mp4" type="video/mp4">
+   
+  </video></div>
+  )
+}
+
+export default Video
